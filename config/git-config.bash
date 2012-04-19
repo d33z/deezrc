04c4aab796e154bb016b617fd4b966fa5b4c1b5c
Matthew Delaney <mdelaney7@gmail.com>
2012-04-18 19:42:15 -0700

    Added basic git config script

---
+++ config/git-config.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "--- General Git Configuration ---"
+echo " "
+
+read -p "Enter your full name: " PRETTY_NAME
+read -p "Enter your email address: " EMAIL
+
+echo "Setting name/email defaults."
+git config --global user.name "$PRETTY_NAME"
+git config --global user.email "$EMAIL"
+
+echo "Colorizing common git commands."
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+
+echo " "
+echo "--- WebKit Git Configuration"
+echo "See: http://trac.webkit.org/wiki/UsingGitWithWebKit"
+echo " "
+
+echo "Setting your WebKit bugzilla username to your email."
+git config --global bugzilla.username "$EMAIL"
+read -p "Enter your bugzilla password for that account: " BUGZILLA_PASS
+git config --global bugzilla.password "$BUGZILLA_PASS"
+
+echo "Setting preference for WebKit scripts to append git branch name to every build."
+git config core.webKitBranchBuild true
